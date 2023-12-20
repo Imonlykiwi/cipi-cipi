22762713f04bc153326ef5be91df21918bd6ab11
Imonlykiwi <112689961+Imonlykiwi@users.noreply.github.com>
2023-12-20 10:59:21 +0100

    -comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,29 @@
 "use strict";
 
-//import class
+// Import the TextCarousel class from the 'utils.js' file
 import { TextCarousel } from './utils.js';
 
-//initialize class
+// Initialize an instance of the TextCarousel class with the specified element reference
 const textCarousel = new TextCarousel("#feature-text");
 
-// Track the number of clicks and redirect to a YouTube link after 10 clicks
+// Function to track the number of clicks and redirect to a YouTube link after 10 clicks
 const trackAndRedirect = (() => {
   let clicks = 0;
 
   return () => {
+    // Increment the click count
     clicks++;
 
-    clicks === 10
-      ? (window.location.href = "https://www.youtube.com/watch?v=QaMySFc-Rec")
-      : console.log(clicks);
+    // Check if the click count has reached 10
+    if (clicks === 10) {
+      // Redirect to the specified YouTube link
+      window.location.href = "https://www.youtube.com/watch?v=QaMySFc-Rec";
+    } else {
+      // Log the current click count if less than 10
+      console.log(clicks);
+    }
   };
 })();
 
+// Add a click event listener to the entire body, triggering the trackAndRedirect function on each click
 document.body.addEventListener("click", trackAndRedirect);
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,8 +1,10 @@
 "use strict";
 
+// Class definition for TextCarousel
 export class TextCarousel {
+  // Constructor takes an element reference as a parameter
   constructor(elementRef) {
-    // Create an instance of TextCarousel
+    // Array of text objects with properties: text, color, time, timeout
     this.carouselText = [
       { text: "Cipi Cipi", color: "white", time: 300, timeout: false },
       { text: "Chapa Chapa", color: "orange", time: 300, timeout: false },
@@ -10,12 +12,17 @@ export class TextCarousel {
       { text: "Daba Daba", color: "orange", time: 300, timeout: false },
       { text: "Magico Mi", color: "white", time: 300, timeout: false },
       { text: "Dubi Dubi", color: "orange", time: 300, timeout: false },
-      { text: "BOOM BOOM!", color: "yellow", time: 600, timeout: true },
+      { text: "BOOM BOOM!", color: "yellow", time: 750, timeout: true },
     ];
+    
+    // Reference to the HTML element where the text will be displayed
     this.elementRef = elementRef;
-    this.init(); //initialize animation
+    
+    // Initialize the text carousel animation
+    this.init();
   }
 
+  // Type a sentence with optional delay between each letter
   async typeSentence(sentence, delay = 30) {
     const letters = sentence.split("");
     let i = 0;
@@ -27,6 +34,7 @@ export class TextCarousel {
     // Append the span element to the specified elementRef
     document.querySelector(this.elementRef).appendChild(spanElement);
 
+    // Type each letter with a delay
     while (i < letters.length) {
       await this.waitForMs(delay);
       spanElement.textContent += letters[i];
@@ -34,9 +42,13 @@ export class TextCarousel {
     }
   }
 
+  // Delete the currently displayed sentence
   async deleteSentence() {
+    // Get the current sentence from the HTML element
     const sentence = document.querySelector(this.elementRef).querySelector('span').innerHTML;
     const letters = sentence.split("");
+
+    // Delete each letter with a delay
     while (letters.length > 0) {
       await this.waitForMs(30);
       letters.pop();
@@ -44,32 +56,44 @@ export class TextCarousel {
     }
   }
 
+  // Start the text carousel animation
   async startCarousel() {
-    console.log("startCarousel called");
     let i = 0;
+
+    // Continuously loop through the carouselText array
     while (true) {
+      // Update the font color based on the current text object
       this.updateFontColor(this.carouselText[i].color);
+
+      // Type the current sentence
       await this.typeSentence(this.carouselText[i].text);
 
+      // If timeout is true, display the text twice with a delay
       if (this.carouselText[i].timeout) {
-        // Display text2 twice with a 50ms delay in between
         for (let j = 1; j < 2; j++) {
-          await this.waitForMs(50);
+          await this.waitForMs(100);
           await this.typeSentence(this.carouselText[i].text);
         }
       }
 
+      // Wait for a specified time, then delete the sentence
       await this.waitForMs(this.carouselText[i].time);
       await this.deleteSentence();
+
+      // Wait for a short time before moving to the next sentence
       await this.waitForMs(this.carouselText[i].time - 50);
-      i = (i + 1) % this.carouselText.length; // Use modulo to loop back to the beginning
+
+      // Use modulo to loop back to the beginning of the array
+      i = (i + 1) % this.carouselText.length;
     }
   }
 
+  // Update the font color of the text element
   updateFontColor(color) {
     document.querySelector(this.elementRef).style.color = color;
   }
 
+  // Helper function to wait for a specified number of milliseconds
   async waitForMs(ms) {
     return new Promise((resolve) => {
       const start = performance.now();
@@ -84,8 +108,8 @@ export class TextCarousel {
     });
   }
 
+  // Initialize the text carousel when the DOM content is loaded
   init() {
-    // Trigger the carousel when the DOM content is loaded
     document.addEventListener("DOMContentLoaded", () => {
       this.startCarousel();
     });
